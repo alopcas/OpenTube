3159be3ccb828fe03f1364186273d1ffff67b386
lope115 <lope115@BD-C31450-O.disversmold.net>
2015-11-13 09:07:25 +0100

    GPL added

--- src/main/java/com/codejumble/opentube/downloader/DownloadManager.java
+++ src/main/java/com/codejumble/opentube/downloader/DownloadManager.java
@@ -14,8 +14,8 @@
  * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with OpenTube. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * OpenTube. If not, see <http://www.gnu.org/licenses/>.
  * *****************************************************************************
  */
 package com.codejumble.opentube.downloader;
